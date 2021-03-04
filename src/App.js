09bfaf00bce85876c7a11d95ed614dd9712f2bf9
moshiur9185 <moshiur9185@gmail.com>
2021-03-04 09:03:31 +0600

    date add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@material-ui/core";
+import { useState, useEffect } from "react";
+import "./App.css";
+import News from "./Components/News/News";
+import Status from "./Components/Status/Status";
+import axios from "axios";
 
 function App() {
+  const [status, setStatus] = useState([]);
+  // useEffect(() => {
+  //   const url =
+  //     "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=485e630ae8cb448dafe2c98c8627abb5";
+  //   fetch(url)
+  //     .then((res) => res.json())
+  //     .then((data) => setStatus(data.articles));
+  // }, []);
+
+  useEffect(() => {
+    const url =
+      "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=485e630ae8cb448dafe2c98c8627abb5";
+    axios(url).then((data) => setStatus(data.data.articles));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="contained" color="secondary">
+        Hello World
+      </Button>
+      <News></News>
+      <h4>Heading: {status.length}</h4>
+      {status.map((articles) => (
+        <Status news={articles}></Status>
+      ))}
     </div>
   );
 }
